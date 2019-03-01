d29e09f3ae711d53dffafae1b55f442b7af52118
2019-DEV-116 <attractuple@gmail.com>
2019-03-01 15:19:05 +0530

    Single Minutes Row - Test with different values

--- BerlinClockTests/BerlinClockTests.swift
+++ BerlinClockTests/BerlinClockTests.swift
@@ -20,10 +20,27 @@ class BerlinClockTests: XCTestCase {
     func testOneMinuteBlockResultForGiveTime() {
         let testOne = dateFormatter!.date(from: "23:59:59")
         let testOneExpectedResult = [cellType.Yellow,cellType.Yellow,cellType.Yellow,cellType.Yellow]
+        let testTwo = dateFormatter!.date(from: "00:00:00")
+        let testTwoExpectedResult = [cellType.Blank,cellType.Blank,cellType.Blank,cellType.Blank]
+        let testThree = dateFormatter!.date(from: "12:32:00")
+        let testThreeExpectedResult = [cellType.Yellow,cellType.Yellow,cellType.Blank,cellType.Blank]
+        let testFour = dateFormatter!.date(from: "12:34:00")
+        let testFourExpectedResult = [cellType.Yellow,cellType.Yellow,cellType.Yellow,cellType.Yellow]
+        let testFive = dateFormatter?.date(from: "12:35:00")
+        let testFiveExpectedResult = [cellType.Blank,cellType.Blank,cellType.Blank,cellType.Blank]
         
-        let valuesForSingleMinutesRowArr = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testOne!)
+        let valuesForSingleMinutesRowArrTestOne = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testOne!)
+        let valuesForSingleMinutesRowArrTestTwo = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testTwo!)
+        let valuesForSingleMinutesRowArrTestThree = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testThree!)
+        let valuesForSingleMinutesRowArrTestFour = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testFour!)
+        let valuesForSingleMinutesRowArrTestFive = berlinCalendarPresenter.valuesForSingleMinutesRow(time:testFive!)
         
-        XCTAssertEqual(valuesForSingleMinutesRowArr, testOneExpectedResult)
+        XCTAssertEqual(valuesForSingleMinutesRowArrTestOne, testOneExpectedResult)
+        XCTAssertEqual(valuesForSingleMinutesRowArrTestTwo, testTwoExpectedResult)
+        XCTAssertEqual(valuesForSingleMinutesRowArrTestThree, testThreeExpectedResult)
+        XCTAssertEqual(valuesForSingleMinutesRowArrTestFour, testFourExpectedResult)
+        XCTAssertEqual(valuesForSingleMinutesRowArrTestFive, testFiveExpectedResult)
+        XCTAssertNotEqual(valuesForSingleMinutesRowArrTestOne, testTwoExpectedResult)
     }
 
 }
