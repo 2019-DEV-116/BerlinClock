0dce6453b9931be95bf4915e041ce347d4b1d588
2019-DEV-116 <attractuple@gmail.com>
2019-03-01 15:10:59 +0530

    Pass Test - Single Minutes Row

--- BerlinClock/BerlinCalendarPresenter.swift
+++ BerlinClock/BerlinCalendarPresenter.swift
@@ -3,8 +3,21 @@ import Foundation
 
 class BerlinCalendarPresenter{
     
+    let SINGLE_MINUTE_CELL_COUNT = 4
+    
     func valuesForSingleMinutesRow(time : Date) -> [BerlinClockUICellType]{
-        let valuesForSingleMinutesRowArr = [BerlinClockUICellType](repeating: .Blank, count: 4)
+        var valuesForSingleMinutesRowArr = [BerlinClockUICellType](repeating: .Blank, count: SINGLE_MINUTE_CELL_COUNT)
+        let calendarCurrent = Calendar.current
+        let calendarComponent =  calendarCurrent.dateComponents([.hour,.minute,.second], from : time)
+        let minute = calendarComponent.minute
+        let oneMinute = minute! % 5
+        for (index,_) in valuesForSingleMinutesRowArr.enumerated() {
+            if (index < oneMinute) {
+                valuesForSingleMinutesRowArr[index] = BerlinClockUICellType.Yellow
+            }else{
+                break
+            }
+        }
         return valuesForSingleMinutesRowArr
     }
 }
